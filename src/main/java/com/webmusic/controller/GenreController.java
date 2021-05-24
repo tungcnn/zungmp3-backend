81be401a9a602082f7de6353e7100ea731bb7d1b
DuyTran1811 <ferssken99@gmail.com>
2021-05-24 14:29:10 +0700

    Not yet

--- src/main/java/com/webmusic/controller/GenreController.java
+++ src/main/java/com/webmusic/controller/GenreController.java
@@ -3,7 +3,6 @@ package com.webmusic.controller;
 import com.webmusic.model.Genre;
 import com.webmusic.service.genre.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.OK;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/genres")
 public class GenreController {
-    @Autowired
     private GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
 
     @GetMapping
     public ResponseEntity<List<Genre>> getAll() {
-        return new ResponseEntity<>(genreService.getAll(), HttpStatus.OK);
+        return new ResponseEntity<>(genreService.getAll(),OK);
     }
+
+
 }
